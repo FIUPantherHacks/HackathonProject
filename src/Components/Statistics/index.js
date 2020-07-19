be42794c680393208334af9e06f9ef3cc9f57c19
Help <pczerokey@gmail.com>
2020-07-18 20:08:15 -0400

    lets tru to chart it up

--- src/Components/Statistics/index.js
+++ src/Components/Statistics/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import Header from "../Header/Header";
+import API from "../../Services/API"
+import { LineChart, Line } from 'recharts';
+const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
+
+
+
 function Statistics() {
+
     return (
         <>
         <Header/>
+
+            {const renderLineChart = (
+            <LineChart width={400} height={400} data={data}>
+                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+            </LineChart>}
         </>
     );
 }
