6a18fd5cb59e58a7ced9b85e123d91672d62a5bd
Help <pczerokey@gmail.com>
2020-07-18 23:03:11 -0400

    api calls quad?

--- src/Components/Statistics/index.js
+++ src/Components/Statistics/index.js
@@ -24,7 +24,7 @@ function  Statistics() {
         }
 
         getNewData();
-    });
+    }, []);
     console.log(data);
     return (
         <>
--- src/Services/API.js
+++ src/Services/API.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const key = '8126c90f-d8a6-4c30-b99c-c89b0cd490a7';
 
 const instance = axios.create({
-    baseURL: 'https://www.cyberpurge.com',
+    baseURL: 'https://cors-anywhere.herokuapp.com/cyberpurge.com',
     timeout: 1000,
     headers: {'X-Authorization' : key}
 });
@@ -29,7 +29,7 @@ const API = {
     globalData: async function () {
         try {
             let response = await instance.get(`/api/covid/globalData`);
-            console.log('Martn', response);
+            console.log('API SERVICED', response);
             return response.data;
         } catch (e) {
             return { message: "There was an Error getting the global Data info" };
